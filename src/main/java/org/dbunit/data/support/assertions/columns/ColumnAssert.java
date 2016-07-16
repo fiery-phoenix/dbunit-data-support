390337450a5ea776186973499e4b98dc8af260cb
Kseniya <kseniya.panasyuk@gmail.com>
2016-07-16 16:44:35 -0400

    added table column hasValues assertion

--- src/main/java/org/dbunit/data/support/assertions/columns/ColumnAssert.java
+++ src/main/java/org/dbunit/data/support/assertions/columns/ColumnAssert.java
@@ -3,34 +3,60 @@ package org.dbunit.data.support.assertions.columns;
 import org.dbunit.data.support.exceptions.DbUnitRuntimeException;
 import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.ITableMetaData;
+import org.dbunit.dataset.datatype.DataType;
+import org.dbunit.dataset.datatype.TypeCastException;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ColumnAssert {
 
-    private final List<Object> values;
+    private final DataType dataType;
+    private final List<Object> actualValues;
 
     public ColumnAssert(ITable table, String column) {
-        values = new ArrayList<>();
+        actualValues = new ArrayList<>();
         try {
+            dataType = getColumnDataType(table, column);
             for (int row = 0; row < table.getRowCount(); row++) {
-                values.add(table.getValue(row, column));
+                actualValues.add(table.getValue(row, column));
             }
         } catch (DataSetException e) {
             throw new DbUnitRuntimeException(e);
         }
     }
 
+    private DataType getColumnDataType(ITable table, String column) throws DataSetException {
+        ITableMetaData tableMetaData = table.getTableMetaData();
+        return tableMetaData.getColumns()[tableMetaData.getColumnIndex(column)].getDataType();
+    }
+
     public void hasNoNullValues() {
-        assertTrue(values.stream().allMatch(Objects::nonNull));
+        assertTrue(actualValues.stream().allMatch(Objects::nonNull));
     }
 
     public void hasOnlyNullValues() {
-        assertTrue(values.stream().noneMatch(Objects::nonNull));
+        assertTrue(actualValues.stream().noneMatch(Objects::nonNull));
     }
 
+    public void hasValues(Object... expectedValues) {
+        assertEquals(expectedValues.length, actualValues.size());
+
+        for (int i = 0; i < expectedValues.length; i++) {
+            assertEqual(expectedValues[i], actualValues.get(i));
+        }
+    }
+
+    private void assertEqual(Object expected, Object actual) {
+        try {
+            assertTrue(dataType.compare(expected, actual) == 0);
+        } catch (TypeCastException e) {
+            throw new DbUnitRuntimeException(e);
+        }
+    }
 }
--- src/test/java/org/dbunit/data/support/assertions/ColumnAssertTest.java
+++ src/test/java/org/dbunit/data/support/assertions/ColumnAssertTest.java
@@ -8,10 +8,12 @@ import static org.dbunit.data.support.DbUnitAssertions.assertThat;
 import static org.dbunit.data.support.DbUnitDataUtils.deleteFrom;
 import static org.dbunit.data.support.DbUnitDataUtils.insert;
 import static org.dbunit.data.support.DbUnitDataUtils.row;
+import static org.dbunit.data.support.generators.ValueGenerators.sequence;
 import static org.dbunit.data.support.tables.tasks.Lists.COMMENT;
 import static org.dbunit.data.support.tables.tasks.Lists.SUMMARY;
 import static org.dbunit.data.support.tables.tasks.TasksTables.LISTS;
 import static org.dbunit.data.support.tables.tasks.TasksTables.USERS;
+import static org.dbunit.data.support.tables.tasks.Users.LOGIN;
 
 public class ColumnAssertTest {
 
@@ -33,6 +35,16 @@ public class ColumnAssertTest {
         insert(LISTS, row().times(5));
         assertThat(LISTS).column(COMMENT).hasOnlyNullValues();
         assertThatThrownBy(() -> assertThat(LISTS).column(SUMMARY).hasOnlyNullValues())
+                .isInstanceOf(AssertionError.class);
+    }
+
+    @Test
+    public void test_hasValues() {
+        insert(USERS, row().withGenerated(LOGIN, sequence("l")).times(3));
+        assertThat(USERS).column(LOGIN).hasValues("l1", "l2", "l3");
+        assertThatThrownBy(() -> assertThat(USERS).column(LOGIN).hasValues("l1", "l2"))
+                .isInstanceOf(AssertionError.class);
+        assertThatThrownBy(() -> assertThat(USERS).column(LOGIN).hasValues("l2", "l1", "l3"))
                 .isInstanceOf(AssertionError.class);
     }
 
